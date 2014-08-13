48bfda60e88cd4afdd5181b5a87babde8424ce54
Nate Turner <nturner@ballytech.com>
2014-08-13 16:46:14 -0700

    messing with deployment

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -14,13 +14,14 @@ set :domain,      "#{application}.com"
 set :deploy_to,   "/var/www/#{domain}"
 
 set :use_composer, true
-set :update_vendors, true
+#set :update_vendors, true
 set :copy_vendors, true
 
 set :writable_dirs,       ["app/cache", "app/logs"]
 set :webserver_user,      "www-data"
 set :permission_method,   :acl
 set :use_set_permissions, true
+set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,     [app_path + "/cache", app_path + "/logs", web_path + "/uploads", "vendor"]
 set :clear_controllers,     true
 set :controllers_to_clear, ['app_*.php']
